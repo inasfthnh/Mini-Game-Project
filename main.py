613e2f4f70d86c56a1d85dd93072afc99f28bdcc
inasfthnh <inasfathinahsa@Gmail.com>
2026-05-25 14:16:32 +1000

    update

--- main.py
+++ main.py
@@ -87,17 +87,11 @@ def run_game() -> None:
     # ── ROUND 1: Suit Jawa ──
     show_round_transition(1, "Suit Jawa")
     SuitJawa.print_intro()
-    print(list_player("survived"))
-    print(list_player("eliminated"))
-    print(list_player("registered"))
     for player_no in list_player("registered"):
         player = Player(player_no)
         suit_game = SuitJawa(player.player_no).play()
         player.record("Suit Jawa", suit_game)
         save_player(player.player_no, suit_game)
-        print(list_player("survived"))
-        print(list_player("eliminated"))
-        print(list_player("registered"))
 
     display_playersboard()
 
@@ -121,9 +115,6 @@ def run_game() -> None:
         bekel_game = BekelChallenge(player.player_no).play()
         player.record("Bekel Challenge", bekel_game)
         save_player(player.player_no, bekel_game)
-        print(list_player("survived"))
-        print(list_player("eliminated"))
-        print(list_player("registered"))
 
     display_playersboard()
 
@@ -147,9 +138,6 @@ def run_game() -> None:
         engklek_game  = EngklekSurvival(player.player_no).play()
         player.record("Engklek Survival", engklek_game)
         save_player(player.player_no, engklek_game)
-        print(list_player("survived"))
-        print(list_player("eliminated"))
-        print(list_player("registered"))
 
     display_playersboard()
 
